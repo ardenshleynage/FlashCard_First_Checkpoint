0a55eb44c66e7119d4eb100de53242d9d903abc6
Ardenshley Nage <nageardenshley@gmail.com>
2025-08-28 14:38:04 -0400

    17th commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 📚 Flashcard App (Tap-to-Reveal)
+
+Une petite application Android permettant d’apprendre grâce à des flashcards.  
+L’utilisateur voit une **question** et peut **appuyer dessus** pour révéler la **réponse**.  
+
+## ✨ Fonctionnalités
+- Affichage d’une question principale
+- Révélation de la réponse au toucher
+- Interface simple et épurée
+- Développée avec **Kotlin** et **Android Studio**
+
+## 🛠️ Technologies utilisées
+- **Kotlin** pour le développement Android
+- **Android Studio** comme IDE
+- Layouts XML pour l’interface utilisateur
+
+## 🚀 Objectif
+Ce projet a été réalisé dans un but d’apprentissage afin de se familiariser avec :  
+- La gestion des événements utilisateurs (clics)  
+- Les bases de l’affichage dynamique dans Android  
+- Le développement mobile avec Kotlin  
---
+++ app/src/main/java/com/org/flashcard_first_checkpoint/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.org.flashcard_first_checkpoint
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.flashcard_question).setOnClickListener {
+            findViewById<View>(R.id.flashcard_question).visibility = View.INVISIBLE
+            findViewById<View>(R.id.flashcard_answer).visibility = View.VISIBLE
+        }
+
+        findViewById<View>(R.id.flashcard_answer).setOnClickListener {
+            findViewById<View>(R.id.flashcard_question).visibility = View.VISIBLE
+            findViewById<View>(R.id.flashcard_answer).visibility = View.INVISIBLE
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
